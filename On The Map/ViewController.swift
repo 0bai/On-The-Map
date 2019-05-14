fe54669149e50814d94b7e84f50a96990db8f8cc
Obai Alnajjar <obai@outlook.com>
2019-05-14 08:55:32 +0300

    Initial Commit

---
+++ On The Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Obai Alnajjar on 5/14/19.
+//  Copyright © 2019 Obai Alnajjar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
