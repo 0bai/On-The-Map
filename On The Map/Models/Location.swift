5095150cd6c307ef192c5193c98dcc491b21bff3
Obai Alnajjar <obai@outlook.com>
2019-05-15 05:56:56 +0300

    Created the Location Model

---
+++ On The Map/Models/Location.swift
@@ -0,0 +1,33 @@
+//
+//  Location.swift
+//  On The Map
+//
+//  Created by Obai Alnajjar on 5/15/19.
+//  Copyright © 2019 Obai Alnajjar. All rights reserved.
+//
+
+import Foundation
+
+struct Location : Codable{
+    var id:String
+    var key:String
+    var firstName:String
+    var lastName:String
+    var geocode:String
+    var website:String
+    var latitude:Float
+    var longitude:Float
+    
+    enum CodingKeys: String, CodingKey{
+        case id = "objectId"
+        case key = "uniqueKey"
+        case firstName
+        case lastName
+        case geocode = "mapString"
+        case website = "mediaURL"
+        case latitude
+        case longitude
+    }
+    
+    
+}
