0f464ba2c1d329b0e8f7cdd969f55d7a7d956a0f
Obai Alnajjar <obai@outlook.com>
2019-05-15 06:06:03 +0300

    Created the Udacian model

---
+++ On The Map/Models/Udacian.swift
@@ -0,0 +1,23 @@
+//
+//  Udacian.swift
+//  On The Map
+//
+//  Created by Obai Alnajjar on 5/15/19.
+//  Copyright © 2019 Obai Alnajjar. All rights reserved.
+//
+
+import Foundation
+
+struct Udacian {
+    var id:String
+    var firstName:String
+    var lastName:String
+    var email:String
+    var password:String
+    
+    struct session {
+        var id:String
+        var expiration:String
+    }
+    
+}
