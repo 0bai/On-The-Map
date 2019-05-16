e73a46050e7e28bf6bd30b3b6531fd370f5ab411
Obai Alnajjar <obai@outlook.com>
2019-05-16 05:31:09 +0300

    Created on the map api struct

---
+++ On The Map/Models/OnTheMapAPI.swift
@@ -0,0 +1,16 @@
+//
+//  OnTheMapAPI.swift
+//  On The Map
+//
+//  Created by Obai Alnajjar on 5/16/19.
+//  Copyright © 2019 Obai Alnajjar. All rights reserved.
+//
+
+import Foundation
+
+struct OnTheMapAPI {
+    static let scheme = "https"
+    static let host = "https://onthemap-api.udacity.com/v1"
+    static let usersPath = "/users"
+    static let sessionPath = "/session"
+}
