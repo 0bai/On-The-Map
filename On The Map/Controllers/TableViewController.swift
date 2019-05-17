b8659eac2136fb951270aafabda58aec58e1574d
Obai Alnajjar <obai@outlook.com>
2019-05-17 14:14:59 +0300

    Added show locations functionality to the table view

--- On The Map/Controllers/TableViewController.swift
+++ On The Map/Controllers/TableViewController.swift
@@ -8,87 +8,60 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
-
-    @IBOutlet weak var logoutButton: UIBarButtonItem!
-    @IBOutlet weak var refreshButton: UIBarButtonItem!
+class TableViewController: UITableViewController, ConnectionDelegate {
+    
+    
+    
+    
+    let cellIdentifier = "addressCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if OnTheMapAPI.studentsLocation == nil {
+            retrieveData()
+        }else{
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        }
+        
     }
     
+    @IBAction func refresh(_ sender: Any) {
+    }
+    @IBAction func logout(_ sender: Any) {
+    }
     
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return OnTheMapAPI.studentsLocation?.results.count ?? 0
     }
-
-    /*
+    
+    func serverError(error: String) {
+        
+    }
+    
+    func listRetrieved() {
+        self.refresh(self)
+    }
+    
+    func retrieveData(){
+        ConnectionManager.connectionDelegate = self
+        ConnectionManager.getLocations()
+    }
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! TableViewCell
+        
+        let student = OnTheMapAPI.studentsLocation?.results[indexPath.row]
+        
+        cell.nameLabel.text = "\(student!.firstName) \(student!.lastName)"
+        
+        cell.websiteLabel.text = student!.website
 
-        // Configure the cell...
 
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
