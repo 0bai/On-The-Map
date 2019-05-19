a244f4c2e9bc641c97a9e7698b6da10771acf3a8
Obai Alnajjar <obai@outlook.com>
2019-05-19 04:59:04 +0300

    Fixed a bug where the url would be constructed without the queries

--- On The Map/Models/MapLocationsManager.swift
+++ On The Map/Models/MapLocationsManager.swift
@@ -16,8 +16,9 @@ extension ConnectionManager{
         parseURL.scheme = ParseAPI.scheme
         parseURL.host = ParseAPI.host
         parseURL.path = ParseAPI.path
+        parseURL.queryItems = []
         ParseAPI.queries.forEach{ key , val in parseURL.queryItems?.append(URLQueryItem(name: key, value: val))}
-        
+
         let headers = [
             "X-Parse-Application-Id":"QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr",
             "X-Parse-REST-API-Key":"QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"]
