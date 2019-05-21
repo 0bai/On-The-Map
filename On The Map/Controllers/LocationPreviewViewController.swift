8ea303e07bfdc9cd89a67ccc4eea8df09a7c6f66
Obai Alnajjar <obai@outlook.com>
2019-05-21 10:35:18 +0300

    Added preview location functionality

--- On The Map/Controllers/LocationPreviewViewController.swift
+++ On The Map/Controllers/LocationPreviewViewController.swift
@@ -7,25 +7,35 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class LocationPreviewViewController: UIViewController {
 
+    @IBOutlet weak var map: MKMapView!
     @IBOutlet weak var finishButton: UIButton!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    
+    var location : CLPlacemark? = nil
+    var website : String = ""
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        updateCorners(obj: finishButton)
+        
+        let annotation = MKPointAnnotation()
+        let user = ConnectionManager.udacian?.udacity.user
+        annotation.coordinate = (location?.location!.coordinate)!
+        annotation.title = "\(user?.firstName ?? "") \(user?.lastName ?? "")"
+        annotation.subtitle = website
+        map.addAnnotation(annotation)
     }
-    */
+    
+    func updateCorners(obj: UIView){
+        obj.layer.cornerRadius = 5
+        obj.clipsToBounds = true
+    }
 
+    @IBAction func finish(_ sender: Any) {
+    }
+    
 }
