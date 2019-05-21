111a4afd8a533358c9ef0532d4ef63b7b3dbe42c
Obai Alnajjar <obai@outlook.com>
2019-05-21 13:30:03 +0300

    Created overwrite dialog

--- On The Map/Views/Alerts.swift
+++ On The Map/Views/Alerts.swift
@@ -10,12 +10,21 @@ import Foundation
 import UIKit
 
 class Alert{
+    
     public static func show(title:String, message: String, sender: UIViewController, completion: @escaping ()->()){
         
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: {
             action in completion()
         }))
+        sender.present(alert, animated: true, completion: nil)
+    }
+    
+    public static func overwriteDialog(sender: UIViewController){
+        let user = ConnectionManager.udacian?.udacity.user
+        let alert = UIAlertController(title: "Warning", message:  "User \"\(user?.firstName ?? "") \(user?.lastName ?? "")\" Has Already Posted a Student Location. Would You Like to Overwrite Their Location?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Overwrite", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         sender.present(alert, animated: true, completion: nil)
     }
 }
